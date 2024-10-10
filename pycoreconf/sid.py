af5b49c3914b65ff3143b2ce89b2dbaf9b9a59e8
Manoj G <manoj.p.gudi@gmail.com>
2024-10-10 13:23:20 +0200

    Fixes for pycoreconf to work with coreconf-webservice

--- pycoreconf/sid.py
+++ pycoreconf/sid.py
@@ -5,8 +5,8 @@ class ModelSID:
     Class to define methods for reading a YANG model SID file and hold values.
     """
 
-    def __init__(self, *sid_files):
-        self.sid_files = sid_files
+    def __init__(self, sid_file):
+        self.sid_file = sid_file
         self.sids, self.types, self.name = self.getSIDsAndTypes() #req. ltn22/pyang
         self.ids = {v: k for k, v in self.sids.items()} # {sid:id}
         self.moduleName = self.getModuleName()
@@ -26,37 +26,28 @@ class ModelSID:
         """
         Read SID file and return { identifier : sid } + { identifier : type } dictionaries.
         """
- 
+        # Read the contents of the sid/json file
+        f = open(self.sid_file, "r")
+        obj = json.load(f)
+        f.close()
+
+        # Get items & map identifier : sid and leafIdentifier : typename
         sids = {} # init
         types = {} # init
-        names = []
-
-        for sid_file in self.sid_files:
-
-            # Read the contents of the sid/json files
-            f = open(sid_file, "r")
-            obj = json.load(f)
-            f.close()
-
-            # Get items & map identifier : sid and leafIdentifier : typename
-            items = obj.get("item") # list
+        items = obj.get("item") # list
         
-            # Old SID models have "items" instead of "item" as key
-            if not items:
-                items = obj["items"] 
+        # Old SID models have "items" instead of "item" as key
+        if not items:
+            items = obj["items"] 
 
-            for item in items:
-                sids[item["identifier"]] = item["sid"]
-                if "type" in item.keys():
-                    types[item["identifier"]] = item["type"]
+        for item in items:
+            sids[item["identifier"]] = item["sid"]
+            if "type" in item.keys():
+                types[item["identifier"]] = item["type"]
 
-            # tmp while single module support:
-            names.append(obj["module-name"])
+        # tmp while single module support:
+        name = obj["module-name"]
 
-        # NOTE IF there are multiple SID files, the names will be concatenated
-        # Concatenate all the names separated by a comma
-        name = ', '.join(names)
-        print("SIDS ", len(sids), len(types))
         return sids, types, name
 
 
@@ -64,17 +55,16 @@ class ModelSID:
         """
         Read SID file and return { sid : identifier } dictionary.
         """
-        for sid_file in self.sid_files:
-            # Read the contents of the sid/json file
-            f = open(sid_file, "r")
-            obj = json.load(f)
-            f.close()
+        # Read the contents of the sid/json file
+        f = open(self.sid_file, "r")
+        obj = json.load(f)
+        f.close()
 
-            # Get items & map identifier : sid
-            ids = {} # init
-            items = obj["items"] # list
-            for item in items:
-                ids[item["sid"]] = item["identifier"]
+        # Get items & map identifier : sid
+        ids = {} # init
+        items = obj["item"] # list
+        for item in items:
+            ids[item["sid"]] = item["identifier"]
 
         return ids
 
@@ -82,18 +72,15 @@ class ModelSID:
         """
         Read SID file and return { identifier : sid } dictionary.
         """
-        sids = {} # init
-        
         # Read the contents of the sid/json file
-        for sid_file in self.sid_files:
-            # Read the contents of the sid/json file
-            f = open(self.sid_file, "r")
-            obj = json.load(f)
-            f.close()
+        f = open(self.sid_file, "r")
+        obj = json.load(f)
+        f.close()
 
-            # Get items & map identifier : sid
-            items = obj["items"] # list
-            for item in items:
-                sids[item["identifier"]] = item["sid"]
+        # Get items & map identifier : sid
+        sids = {} # init
+        items = obj["item"] # list
+        for item in items:
+            sids[item["identifier"]] = item["sid"]
 
         return sids
